96b238c6f2c08f51c84a1e00539f6f75a8ef40cb
stave <zhoukangkang@zhoukangkangdeMacBook-Pro.local>
2017-05-16 09:59:45 +0800

    新建项目

---
+++ InterviewDemo/InterviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterviewDemo
+//
+//  Created by stave on 2017/5/16.
+//  Copyright © 2017年 stave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
